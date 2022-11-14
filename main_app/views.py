9c1621180d0e7df24bf7ca507a08e65e57d83347
notmaxwang <maxwang3737@gmail.com>
2022-11-14 14:57:01 -0800

    implemented sneaker index

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+sneakers = [
+    {'name': 'Jordan 1', 'colorway': 'Bred', 'retail_price': '$190'},
+    {'name': 'Jordan 3', 'colorway': 'Black Cement', 'retail_price': '$220'},
+]
+
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def sneakers_index(request):
+    return render(request, 'sneakers/index.html', {
+        'sneakers': sneakers
+    })
